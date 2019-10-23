df9cdd32083b01d774d9cdd8a1a2244fac7a8864
Avinaba-Ray <avinabaray@gmail.com>
2019-10-23 08:48:28 +0530

    Installed mongoose, mocha, nodemon in pachage.json in dependencies
    On SUBMIT in form_menu.html, 404 not found appears.
    In form_menu.html, ADD MORE button not functional.

--- MenuManagement/models/Resto.js
+++ MenuManagement/models/Resto.js
@@ -5,41 +5,41 @@ const Schema = mongoose.Schema;
 
 // sub document
 const MenuSchema = new Schema({
-  item: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0
-  }
+    item: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    }
 });
 
 const RestoSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  address: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  phone: {
-    type: Number,
-    required: true,
-    minlength: 6
-  },
-  email: {
-    type: String
-    // unique: true
-  },
-  veg: [MenuSchema],
-  nonVeg: [MenuSchema],
-  desserts: [MenuSchema]
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    phone: {
+        type: Number,
+        required: true,
+        minlength: 6
+    },
+    email: {
+        type: String
+        // unique: true
+    },
+    veg: [MenuSchema],
+    nonVeg: [MenuSchema],
+    desserts: [MenuSchema]
 });
 
 module.exports = mongoose.model('Resto', RestoSchema);
